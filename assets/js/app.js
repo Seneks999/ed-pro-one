3b69395c9f9669ab6c68b245e937103625d6170e
Seneks999 <mrseneks@gmail.com>
2024-11-14 08:33:54 +0200

    js nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,49 @@
 $(function () {
   var header = $("#header"),
     introH = $("#intro").innerHeight(),
-    scrollOffset = 0;
+    scrollOffset = $(window).scrollTop();
+
+  /* Fixed header  */
+  checkScroll(scrollOffset);
 
   $(window).on("scroll", function () {
     scrollOffset = $(this).scrollTop();
+    checkScroll(scrollOffset);
+  });
 
+  function checkScroll(scrollOffset) {
     if (scrollOffset >= introH) {
       header.addClass("fixed");
     } else {
       header.removeClass("fixed");
     }
+  }
+
+  /* Smooth scroll */
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+    var $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top;
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset,
+      },
+      500
+    );
+
+    // Закриваємо меню та повертаємо бургер після натискання на пункт меню
+    $("#nav").removeClass("active");
+    $("#nav-toggle").removeClass("active");
+  });
+  /* Menu nav toggle */
+  $("#nav-toggle").on("click", function (event) {
+    event.preventDefault();
+
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
   });
 });
